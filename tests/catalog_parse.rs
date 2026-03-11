53d4056a695eb8ba08ac4ed645407a93a7be3e0a
Qi-Zhan <qizhan@zju.edu.cn>
2026-03-11 15:50:24 +0800

    remove tests

--- tests/catalog_parse.rs
+++
@@ -1,9 +0,0 @@
-use cadenza::catalog::load_catalog;
-
-#[test]
-fn parses_catalog_entries() {
-    let entries = load_catalog("catalog/classics.toml").unwrap();
-
-    assert!(entries.len() >= 8);
-    assert!(entries.iter().any(|entry| entry.composer == "Bach"));
-}
